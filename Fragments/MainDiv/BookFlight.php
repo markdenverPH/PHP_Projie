95d5b805a4e5a8e59d1a23b33e68772ba4730862
passwordjr <angelojrganaden@gmail.com>
2017-06-18 22:56:02 +0800

    Changes

--- Fragments/MainDiv/BookFlight.php
+++ Fragments/MainDiv/BookFlight.php
@@ -2,7 +2,16 @@
 <h1 class="pull-left BannMsg">Book a Flight</h1><br><br><br><br>
 <div class="row">
 	<div class="col-md-4">
+		<div class="dropdown">
+			<button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Dropdown Example
+				<span class="caret"></span></button>
+				<ul class="dropdown-menu">
+					<li><a href="#">HTML</a></li>
+					<li><a href="#">CSS</a></li>
+					<li><a href="#">JavaScript</a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="col-md-4"></div>
+		<div class="col-md-4"></div>
 	</div>
-	<div class="col-md-4"></div>
-	<div class="col-md-4"></div>
-</div>
