7a589ddb0b7bd7cc2e977e76bbb5712231397da5
passwordjr <angelojrganaden@gmail.com>
2017-06-18 19:24:15 +0800

    Added Book Package follow up

---
+++ Fragments/MainDiv/BookPackage.php
@@ -0,0 +1,129 @@
+<h1 class="pull-left">Flight Packages</h1><br><br><br><br>
+<!-- FIRST ROW OF PACKAGES -->
+<div class="row">
+
+  <div class="col-md-4">
+    <div class="card " style="width: 25rem; margin: 20px;">
+      <img class="card-img-top cardImage" src="../img/sample.jpg" alt="Card image cap" >
+      <div class="card-block">
+        <h4 class="card-title">Card title</h4>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">Cras justo odio</li>
+        <li class="list-group-item">Dapibus ac facilisis in</li>
+        <li class="list-group-item">Vestibulum at eros</li>
+      </ul>
+      <div class="card-block">
+        <a href="#" class="card-link">Card link</a>
+        <a href="#" class="card-link">Another link</a>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="col-md-4">
+    <div class="card " style="width: 25rem; margin: 20px;">
+      <img class="card-img-top cardImage" src="../img/sample.jpg" alt="Card image cap" >
+      <div class="card-block">
+        <h4 class="card-title">Card title</h4>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">Cras justo odio</li>
+        <li class="list-group-item">Dapibus ac facilisis in</li>
+        <li class="list-group-item">Vestibulum at eros</li>
+      </ul>
+      <div class="card-block">
+        <a href="#" class="card-link">Card link</a>
+        <a href="#" class="card-link">Another link</a>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="col-md-4">
+    <div class="card " style="width: 25rem; margin: 20px;">
+      <img class="card-img-top cardImage" src="../img/sample.jpg" alt="Card image cap" >
+      <div class="card-block">
+        <h4 class="card-title">Card title</h4>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">Cras justo odio</li>
+        <li class="list-group-item">Dapibus ac facilisis in</li>
+        <li class="list-group-item">Vestibulum at eros</li>
+      </ul>
+      <div class="card-block">
+        <a href="#" class="card-link">Card link</a>
+        <a href="#" class="card-link">Another link</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<!-- SECOND ROW OF PACKAGES -->
+
+
+<div class="row">
+
+  <div class="col-md-4">
+    <div class="card " style="width: 25rem; margin: 20px;">
+      <img class="card-img-top cardImage" src="../img/sample.jpg" alt="Card image cap" >
+      <div class="card-block">
+        <h4 class="card-title">Card title</h4>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">Cras justo odio</li>
+        <li class="list-group-item">Dapibus ac facilisis in</li>
+        <li class="list-group-item">Vestibulum at eros</li>
+      </ul>
+      <div class="card-block">
+        <a href="#" class="card-link">Card link</a>
+        <a href="#" class="card-link">Another link</a>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="col-md-4">
+    <div class="card " style="width: 25rem; margin: 20px;">
+      <img class="card-img-top cardImage" src="../img/sample.jpg" alt="Card image cap" >
+      <div class="card-block">
+        <h4 class="card-title">Card title</h4>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">Cras justo odio</li>
+        <li class="list-group-item">Dapibus ac facilisis in</li>
+        <li class="list-group-item">Vestibulum at eros</li>
+      </ul>
+      <div class="card-block">
+        <a href="#" class="card-link">Card link</a>
+        <a href="#" class="card-link">Another link</a>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="col-md-4">
+    <div class="card " style="width: 25rem; margin: 20px;">
+      <img class="card-img-top cardImage" src="../img/sample.jpg" alt="Card image cap" >
+      <div class="card-block">
+        <h4 class="card-title">Card title</h4>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">Cras justo odio</li>
+        <li class="list-group-item">Dapibus ac facilisis in</li>
+        <li class="list-group-item">Vestibulum at eros</li>
+      </ul>
+      <div class="card-block">
+        <a href="#" class="card-link">Card link</a>
+        <a href="#" class="card-link">Another link</a>
+      </div>
+    </div>
+  </div>
+</div>
