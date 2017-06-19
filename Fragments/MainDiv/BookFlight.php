c3064b0bed89fe0c73714a8f2cb6ca4d134efd84
passwordjr <angelojrganaden@gmail.com>
2017-06-19 10:05:02 +0800

    Changes in BookFlight

--- Fragments/MainDiv/BookFlight.php
+++ Fragments/MainDiv/BookFlight.php
@@ -1,17 +1,24 @@
 
 <h1 class="pull-left BannMsg">Book a Flight</h1><br><br><br><br>
 <div class="row">
+	<div class="col-md-4"></div>
 	<div class="col-md-4">
-		<div class="dropdown">
-			<button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Dropdown Example
-				<span class="caret"></span></button>
-				<ul class="dropdown-menu">
-					<li><a href="#">HTML</a></li>
-					<li><a href="#">CSS</a></li>
-					<li><a href="#">JavaScript</a></li>
-				</ul>
-			</div>
+		<div class="form-group">
+			<label> Origin</label>
+			<select style="width: 100%;">
+				<?php include'BookFlight-Destinations.php'; ?>
+			</select>
 		</div>
-		<div class="col-md-4"></div>
-		<div class="col-md-4"></div>
+		<div class="form-group">
+			<label> Destinaton</label>
+			<select style="width: 100%;">
+				<?php include'BookFlight-Destinations.php'; ?>
+			</select>
+		</div>
 	</div>
+	<div class="col-md-4"></div>
+</div>
+
+<script type="text/javascript">
+	
+</script>
