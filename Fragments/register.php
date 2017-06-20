6a03d625804a75a042e20c45a6bf4c40bb75a350
markdenverPH <markdenverbabaran@gmail.com>
2017-06-20 18:12:35 +0800

    message here

--- Fragments/register.php
+++ Fragments/register.php
@@ -4,37 +4,37 @@ include("conn.php");
 
 
 if (isset($_POST['register-submit'])) {
-  $firstname = $_POST['firstname'];
-  $middlename = $_POST['middlename'];
-  $lastname = $_POST['lastname'];
-  $age = $_POST['age'];
-  $gender = $_POST['gender'];
-  $email = $_POST['email'];
-  $address = $_POST['address'];
-  $password = $_POST['password_input'];
-  $booleanInsert = true;
+     $firstname = $_POST['firstname'];
+     $middlename = $_POST['middlename'];
+     $lastname = $_POST['lastname'];
+     $age = $_POST['age'];
+     $gender = $_POST['gender'];
+     $email = $_POST['email'];
+     $address = $_POST['address'];
+     $password = $_POST['password_input'];
+     $booleanInsert = true;
 
-  $dupesql = "SELECT * FROM passenger where ( (pass_firstname ='$firstname' AND pass_middlename = '$middlename' AND pass_lastname = '$lastname') OR pass_email = '$email'   )";
+     $dupesql = "SELECT * FROM passenger where ( (pass_firstname ='$firstname' AND pass_middlename = '$middlename' AND pass_lastname = '$lastname') OR pass_email = '$email'   )";
 
-  $checkdup = mysqli_query($con,$dupesql);
+     $checkdup = mysqli_query($con,$dupesql);
 
-  if (mysqli_num_rows($checkdup) > 0) {
-    $booleanInsert = false;
-  }
+     if (mysqli_num_rows($checkdup) > 0) {
+          $booleanInsert = false;
+     }
 
 
-  $query_upload="
-  INSERT into passenger (pass_age,pass_address,pass_gender,pass_firstname,pass_middlename,pass_lastname,pass_email,pass_password) 
-  VALUES('".$age." ','".$address."',"."'".$gender."',"."'".$firstname."',"."'".$middlename."','".$lastname."',"."'".$email."',"."'".$password."')";
+     $query_upload="
+     INSERT into passenger (pass_age,pass_address,pass_gender,pass_firstname,pass_middlename,pass_lastname,pass_email,pass_password)
+     VALUES('".$age." ','".$address."',"."'".$gender."',"."'".$firstname."',"."'".$middlename."','".$lastname."',"."'".$email."',"."'".$password."')";
 
-  if ($booleanInsert == true) {
-   if (mysqli_query($con, $query_upload) or die("error in $query_upload == ----> ".mysqli_error($con))){
-    echo "success";
-  }else{
-    echo "failes";
-  }
-}else{
-  echo "dup account";
-}
+     if ($booleanInsert == true) {
+          if (mysqli_query($con, $query_upload) or die("error in $query_upload == ----> ".mysqli_error($con))){
+               echo "success";
+          }else{
+               echo "failes";
+          }
+     }else{
+          echo "dup account";
+     }
 }
 ?>
