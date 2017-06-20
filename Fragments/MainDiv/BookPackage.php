06b33fa2fb3b547ed4a8cabea2735e4c2b3167c5
markdenverPH <markdenverbabaran@gmail.com>
2017-06-20 23:30:57 +0800

    landing page error

--- Fragments/MainDiv/BookPackage.php
+++ Fragments/MainDiv/BookPackage.php
@@ -3,162 +3,215 @@
 <!-- FIRST ROW OF PACKAGES -->
 <div class="row">
 
-  <div class="col-md-4 ">
-    <div class="card " style="width: 25rem; margin: 20px;">
-      <img class="card-img-top cardImage" src="../img/sample.jpg" alt="Card image cap" >
-      <div class="card-block">
-        <h4 class="card-title">Happy Family Four in Cebu!</h4>
-        <p class="card-text">A perfect bundle for a Family of four!</p>
-      </div>
-
-      <ul class="list-group list-group-flush">
-        <li class="list-group-item listClassHead">
-          <span class="glyphicon glyphicon-plane HeadGlyph"></span>Manila - Cebu</li>
-          <li class="list-group-item listClass">Roundtrip</li>
-          <li class="list-group-item listClass">
-            <span >Hotel + Transportation</span>
-          </li>
-          <li class="list-group-item listClass">P000000</li>
-          <li class="list-group-item" >
-            <center>
-              <input type="submit" name="" value="Checkout" class="btn btn-primary">
-            </center>
-          </li>
-        </ul>
-      </div>
-    </div>
-
-    <div class="col-md-4 ">
-      <div class="card " style="width: 25rem; margin: 20px;">
-        <img class="card-img-top cardImage" src="../img/sample.jpg" alt="Card image cap" >
-        <div class="card-block">
-          <h4 class="card-title">Happy Family Four in Cebu!</h4>
-          <p class="card-text">A perfect bundle for a Family of four!</p>
-        </div>
-
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item listClassHead">
-            <span class="glyphicon glyphicon-plane HeadGlyph"></span>Manila - Cebu</li>
-            <li class="list-group-item listClass">Roundtrip</li>
-            <li class="list-group-item listClass">
-              <span >Hotel + Transportation</span>
-            </li>
-            <li class="list-group-item listClass">P000000</li>
-            <li class="list-group-item" >
-              <center>
-                <input type="submit" name="" value="Checkout" class="btn btn-primary">
-              </center>
-            </li>
-          </ul>
-        </div>
-      </div>
-
-      <div class="col-md-4 ">
-        <div class="card " style="width: 25rem; margin: 20px;">
-          <img class="card-img-top cardImage" src="../img/sample.jpg" alt="Card image cap" >
-          <div class="card-block">
-            <h4 class="card-title">Happy Family Four in Cebu!</h4>
-            <p class="card-text">A perfect bundle for a Family of four!</p>
-          </div>
-
-          <ul class="list-group list-group-flush">
-            <li class="list-group-item listClassHead">
-              <span class="glyphicon glyphicon-plane HeadGlyph"></span>Manila - Cebu</li>
-              <li class="list-group-item listClass">Roundtrip</li>
-              <li class="list-group-item listClass">
-                <span >Hotel + Transportation</span>
-              </li>
-              <li class="list-group-item listClass">P000000</li>
-              <li class="list-group-item" >
-                <center>
-                  <input type="submit" name="" value="Checkout" class="btn btn-primary">
-                </center>
-              </li>
-            </ul>
-          </div>
-        </div>
-
-
-      </div>
-
-
-      <!-- SECOND ROW OF PACKAGES -->
-      <div class="row">
-
-        <div class="col-md-4 ">
+     <div class="col-md-4 " align="center">
           <div class="card " style="width: 25rem; margin: 20px;">
-            <img class="card-img-top cardImage" src="../img/sample.jpg" alt="Card image cap" >
-            <div class="card-block">
-              <h4 class="card-title">Happy Family Four in Cebu!</h4>
-              <p class="card-text">A perfect bundle for a Family of four!</p>
-            </div>
+               <img class="card-img-top cardImage" src="../img/pack1.jpg" alt="Card image cap">
+               <div class="card-block">
+                    <h5 class="card-title" align="center">Henann Regency Resort And Spa</h5    >
+                    <p class="card-text">Good for 4 person</p>
+                    <p class="card-text">3 Days, 2 Nights</p>
+               </div>
 
-            <ul class="list-group list-group-flush">
-              <li class="list-group-item listClassHead">
-                <span class="glyphicon glyphicon-plane HeadGlyph"></span>Manila - Cebu</li>
-                <li class="list-group-item listClass">Roundtrip</li>
-                <li class="list-group-item listClass">
-                  <span >Hotel + Transportation</span>
-                </li>
-                <li class="list-group-item listClass">P000000</li>
-                <li class="list-group-item" >
-                  <center>
-                    <input type="submit" name="" value="Checkout" class="btn btn-primary">
-                  </center>
-                </li>
-              </ul>
-            </div>
-          </div>
-
-          <div class="col-md-4 ">
-            <div class="card " style="width: 25rem; margin: 20px;">
-              <img class="card-img-top cardImage" src="../img/sample.jpg" alt="Card image cap" >
-              <div class="card-block">
-                <h4 class="card-title">Happy Family Four in Cebu!</h4>
-                <p class="card-text">A perfect bundle for a Family of four!</p>
-              </div>
-
-              <ul class="list-group list-group-flush">
-                <li class="list-group-item listClassHead">
-                  <span class="glyphicon glyphicon-plane HeadGlyph"></span>Manila - Cebu</li>
-                  <li class="list-group-item listClass">Roundtrip</li>
-                  <li class="list-group-item listClass">
-                    <span >Hotel + Transportation</span>
-                  </li>
-                  <li class="list-group-item listClass">P000000</li>
-                  <li class="list-group-item" >
-                    <center>
-                      <input type="submit" name="" value="Checkout" class="btn btn-primary">
-                    </center>
-                  </li>
-                </ul>
-              </div>
-            </div>
-
-            <div class="col-md-4 ">
-              <div class="card " style="width: 25rem; margin: 20px;">
-                <img class="card-img-top cardImage" src="../img/sample.jpg" alt="Card image cap" >
-                <div class="card-block">
-                  <h4 class="card-title">Happy Family Four in Cebu!</h4>
-                  <p class="card-text">A perfect bundle for a Family of four!</p>
-                </div>
-
-                <ul class="list-group list-group-flush">
-                  <li class="list-group-item listClassHead">
-                    <span class="glyphicon glyphicon-plane HeadGlyph"></span>Manila - Cebu</li>
+               <ul class="list-group list-group-flush">
+                    <li class="list-group-item listClassHead">
+                         <span class="glyphicon glyphicon-plane HeadGlyph"></span>Boracay Island</li>
                     <li class="list-group-item listClass">Roundtrip</li>
                     <li class="list-group-item listClass">
-                      <span >Hotel + Transportation</span>
+                         <span>Hotel + Transportation</span>
+                    </li>
+                    <li class="list-group-item listClass">P18,537.00</li>
+                    <li class="list-group-item">
+                         <center>
+                              <!--<input type="submit" name="" value="Checkout" class="btn btn-primary"
+                              target="#modalpack1" data-toggle="modal">-->
+                              <input type="submit" class="btn btn-primary" data-toggle="modal"
+                              data-target="#modalpack1" value="Checkout"/>
+                         </center>
+                    </li>
+               </ul>
+          </div>
+     </div>
+     <!-- Modal -->
+     <div id="modalpack1" class="modal fade" role="dialog">
+          <div class="modal-dialog">
+               <!-- Modal content-->
+               <div class="modal-content">
+                    <div class="modal-header">
+                         <button type="button" class="close" data-dismiss="modal">&times;</button>
+                         <h4 class="modal-title"><b>Henann Regency Resort And Spa</b></h4>
+                    </div>
+                    <div class="modal-body">
+                         <p>Room Service [24-hour]</p>
+                         <p>Free Wi-Fi Access</p>
+                         <p>Capacity: 4 Persons</p>
+                         <p>2 Beds</p>
+                    </div>
+                    <div class="modal-footer">
+                         <form method="post">
+                              <input type="button" name="checkout_confirm" class="btn btn-primary"
+                              data-dismiss="modal" value="Checkout" onclick="confirmed_checkout()"></input>
+                         </form>
+                    </div>
+               </div>
+          </div>
+     </div>
+     <script>
+          function confirmed_checkout(){
+               alert('Success');
+          }
+     </script>
+     <div class="col-md-4 ">
+          <div class="card " style="width: 25rem; margin: 20px;">
+               <img class="card-img-top cardImage" src="../img/pack2.jpg" alt="Card image cap">
+               <div class="card-block">
+                    <h5 class="card-title">Paradise Island Park & Beach Resort</h5>
+                    <p class="card-text">Good for 6 person</p>
+                    <p class="card-text">3 Days, 2 Nights</p>
+               </div>
+
+               <ul class="list-group list-group-flush">
+                    <li class="list-group-item listClassHead">
+                         <span class="glyphicon glyphicon-plane HeadGlyph"></span>Manila - Cebu</li>
+                    <li class="list-group-item listClass">Roundtrip</li>
+                    <li class="list-group-item listClass">
+                         <span>Hotel + Transportation</span>
+                    </li>
+                    <li class="list-group-item listClass">P20,000.00</li>
+                    <li class="list-group-item">
+                         <center>
+                              <input type="submit" class="btn btn-primary" data-toggle="modal"
+                              data-target="#modalpack2" value="Checkout"/>
+                         </center>
+                    </li>
+               </ul>
+          </div>
+     </div>
+     <!-- Modal -->
+     <div id="modalpack2" class="modal fade" role="dialog">
+          <div class="modal-dialog">
+               <!-- Modal content-->
+               <div class="modal-content">
+                    <div class="modal-header">
+                         <button type="button" class="close" data-dismiss="modal">&times;</button>
+                         <h4 class="modal-title"><b>Paradise Island Park & Beach Resort</b></h4>
+                    </div>
+                    <div class="modal-body">
+                         <p>Room Service [24-hour]</p>
+                         <p>Free Wi-Fi Access</p>
+                         <p>Capacity: 6 Persons</p>
+                         <p>2 Beds</p>
+                    </div>
+                    <div class="modal-footer">
+                         <form method="post">
+                              <input type="button" name="checkout_confirm" class="btn btn-primary"
+                              data-dismiss="modal" value="Checkout" onclick="confirmed_checkout()"></input>
+                         </form>
+                    </div>
+               </div>
+          </div>
+     </div>
+     <div class="col-md-4 ">
+          <div class="card " style="width: 25rem; margin: 20px;">
+               <img class="card-img-top cardImage" src="../img/sample.jpg" alt="Card image cap">
+               <div class="card-block">
+                    <h4 class="card-title">Happy Family Four in Cebu!</h4>
+                    <p class="card-text">A perfect bundle for a Family of four!</p>
+               </div>
+
+               <ul class="list-group list-group-flush">
+                    <li class="list-group-item listClassHead">
+                         <span class="glyphicon glyphicon-plane HeadGlyph"></span>Manila - Cebu</li>
+                    <li class="list-group-item listClass">Roundtrip</li>
+                    <li class="list-group-item listClass">
+                         <span>Hotel + Transportation</span>
                     </li>
                     <li class="list-group-item listClass">P000000</li>
-                    <li class="list-group-item" >
-                      <center>
-                        <input type="submit" name="" value="Checkout" class="btn btn-primary">
-                      </center>
+                    <li class="list-group-item">
+                         <center>
+                              <input type="submit" name="" value="Checkout" class="btn btn-primary">
+                         </center>
                     </li>
-                  </ul>
-                </div>
-              </div>
+               </ul>
+          </div>
+     </div>
+</div>
 
 
-            </div>
+<!-- SECOND ROW OF PACKAGES -->
+<div class="row">
+
+     <div class="col-md-4 ">
+          <div class="card " style="width: 25rem; margin: 20px;">
+               <img class="card-img-top cardImage" src="../img/sample.jpg" alt="Card image cap">
+               <div class="card-block">
+                    <h4 class="card-title">Happy Family Four in Cebu!</h4>
+                    <p class="card-text">A perfect bundle for a Family of four!</p>
+               </div>
+
+               <ul class="list-group list-group-flush">
+                    <li class="list-group-item listClassHead">
+                         <span class="glyphicon glyphicon-plane HeadGlyph"></span>Manila - Cebu</li>
+                    <li class="list-group-item listClass">Roundtrip</li>
+                    <li class="list-group-item listClass">
+                         <span>Hotel + Transportation</span>
+                    </li>
+                    <li class="list-group-item listClass">P000000</li>
+                    <li class="list-group-item">
+                         <center>
+                              <input type="submit" name="" value="Checkout" class="btn btn-primary">
+                         </center>
+                    </li>
+               </ul>
+          </div>
+     </div>
+
+     <div class="col-md-4 ">
+          <div class="card " style="width: 25rem; margin: 20px;">
+               <img class="card-img-top cardImage" src="../img/sample.jpg" alt="Card image cap">
+               <div class="card-block">
+                    <h4 class="card-title">Happy Family Four in Cebu!</h4>
+                    <p class="card-text">A perfect bundle for a Family of four!</p>
+               </div>
+
+               <ul class="list-group list-group-flush">
+                    <li class="list-group-item listClassHead">
+                         <span class="glyphicon glyphicon-plane HeadGlyph"></span>Manila - Cebu</li>
+                    <li class="list-group-item listClass">Roundtrip</li>
+                    <li class="list-group-item listClass">
+                         <span>Hotel + Transportation</span>
+                    </li>
+                    <li class="list-group-item listClass">P000000</li>
+                    <li class="list-group-item">
+                         <center>
+                              <input type="submit" name="" value="Checkout" class="btn btn-primary">
+                         </center>
+                    </li>
+               </ul>
+          </div>
+     </div>
+
+     <div class="col-md-4 ">
+          <div class="card " style="width: 25rem; margin: 20px;">
+               <img class="card-img-top cardImage" src="../img/sample.jpg" alt="Card image cap">
+               <div class="card-block">
+                    <h4 class="card-title">Happy Family Four in Cebu!</h4>
+                    <p class="card-text">A perfect bundle for a Family of four!</p>
+               </div>
+
+               <ul class="list-group list-group-flush">
+                    <li class="list-group-item listClassHead">
+                         <span class="glyphicon glyphicon-plane HeadGlyph"></span>Manila - Cebu</li>
+                    <li class="list-group-item listClass">Roundtrip</li>
+                    <li class="list-group-item listClass">
+                         <span>Hotel + Transportation</span>
+                    </li>
+                    <li class="list-group-item listClass">P000000</li>
+                    <li class="list-group-item">
+                         <center>
+                              <input type="submit" name="" value="Checkout" class="btn btn-primary">
+                         </center>
+                    </li>
+               </ul>
+          </div>
+     </div>
+</div>
