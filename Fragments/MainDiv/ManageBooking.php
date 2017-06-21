e0d26534ca160a8d6bbab7426c1aefe9c9555e37
passwordjr <angelojrganaden@gmail.com>
2017-06-21 11:36:31 +0800

    shoot

--- Fragments/MainDiv/ManageBooking.php
+++ Fragments/MainDiv/ManageBooking.php
@@ -24,29 +24,31 @@
 				</tr>
 			</thead>		
 			<tbody>
-				<tr class="info">
-					<?php
-					While($row3 = mysqli_fetch_array($result)){
 
-						$fl_id = $row3['FL_ID'];
-						$fl_class = $row3['FL_CLASS'];
-						$fl_departdate = $row3['FL_DEPARTDATE'];
-						$fl_returndate = $row3['FL_RETURNDATE'];
-						$fl_from = $row3['FL_FROM'];
-						$fl_to = $row3['FL_TO'];
-						$fl_passqty = $row3['FL_PASSQTY'];
-						$fl_flare = $row3['FL_FARE'];
-						echo "<td> $fl_id </td>";
-						echo "<td> $fl_class </td>";
-						echo "<td>". date("F j, Y ",$fl_departdate)."</td>";
-						echo "<td>". date("F j, Y ",$fl_returndate)."</td>";
-						echo "<td> $fl_from </td>";
-						echo "<td> $fl_to </td>";
-						echo "<td> $fl_passqty Persons </td>";
-						echo "<td> $fl_flare </td>";
-					}
+				<?php
+				While($row3 = mysqli_fetch_array($result)){
+
+
+					$fl_id = $row3['FL_ID'];
+					$fl_class = $row3['FL_CLASS'];
+					$fl_departdate = $row3['FL_DEPARTDATE'];
+					$fl_returndate = $row3['FL_RETURNDATE'];
+					$fl_from = $row3['FL_FROM'];
+					$fl_to = $row3['FL_TO'];
+					$fl_passqty = $row3['FL_PASSQTY'];
+					$fl_flare = $row3['FL_FARE'];
+					echo "	<tr class='info'>";
+					echo "<td> $fl_id </td>";
+					echo "<td> $fl_class </td>";
+					echo "<td>". date("F j, Y ",$fl_departdate)."</td>";
+					echo "<td>". date("F j, Y ",$fl_returndate)."</td>";
+					echo "<td> $fl_from </td>";
+					echo "<td> $fl_to </td>";
+					echo "<td> $fl_passqty Persons </td>";
+					echo "<td> $fl_flare </td>";
+					echo "</tr>";
 				}
-				?>
-			</tr>
+			}
+			?>
 		</tbody>
 	</table>
