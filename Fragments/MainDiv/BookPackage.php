6169e3d73e90d54d3d066c673c7185df861374ac
markdenverPH <markdenverbabaran@gmail.com>
2017-06-21 11:37:39 +0800

    semi-done book package additional

--- Fragments/MainDiv/BookPackage.php
+++ Fragments/MainDiv/BookPackage.php
@@ -160,26 +160,51 @@
 </div>
 
 <div class="row" align="center">
-     <div class="col-md-4 ">
+     <div class="col-md-4" align="center">
           <div class="card " style="width: 25rem; margin: 20px;">
-               <img class="card-img-top cardImage" src="../img/sample.jpg" alt="Card image cap">
+               <img class="card-img-top cardImage" src="../img/pack4.jpg" alt="Card image cap">
                <div class="card-block">
-                    <h4 class="card-title">Happy Family Four in Cebu!</h4>
-                    <p class="card-text">A perfect bundle for a Family of four!</p>
+                    <h5 class="card-title" align="center">Club Tara Resort</h5>
+                    <p class="card-text">Good for 2 person</p>
+                    <p class="card-text">3 Days, 2 Nights</p>
                </div>
 
                <ul class="list-group list-group-flush">
                     <li class="list-group-item listClassHead">
-                         <span class="glyphicon glyphicon-plane HeadGlyph"></span>Manila - Cebu</li>
+                         <span class="glyphicon glyphicon-plane HeadGlyph"></span>Surigago (SUR)</li>
+                    <li class="list-group-item listClass">Location: Surigao del Norte</li>
                     <li class="list-group-item listClass">Roundtrip</li>
                     <li class="list-group-item listClass">
                          <span>Hotel + Transportation</span>
                     </li>
-                    <li class="list-group-item listClass">P000000</li>
-                    <li class="list-group-item">
-                         <center>
-                              <input type="submit" name="" value="Checkout" class="btn btn-primary">
-                         </center>
+                    <li class="list-group-item listClass">July 28 2017 - July 30 2017</li>
+                    <li class="list-group-item listClass">P9,000.00</li>
+                    <li class="list-group-item listClass">
+                         <form method="post" action="MainDiv/BookPackage_Enter.php">
+                              <input type="hidden" name="BP_title1" value="Best Western Plus The Ivywall Hotel"/>
+                              <input type="hidden" name="BP_pax1" value="2"/>
+                              <input type="hidden" name="BP_days1" value="3 Days, 2 Nights2"/>
+                              <input type="hidden" name="BP_airport1" value="Puerto Princesa (PPS)"/>
+                              <input type="hidden" name="BP_location1" value="Puerto Princesa, Palawan"/>
+                              <input type="hidden" name="BP_trip1" value="Roundtrip"/>
+                              <input type="hidden" name="BP_pack1" value="Hotel + Transportation"/>
+                              <input type="hidden" name="BP_sched1" value="July 28 2017 - July 30 2017"/>
+                              <input type="hidden" name="BP_price1" value="9000.00"/>
+                              <div id="divFrom" class="form-group">
+                                   <label> Origin</label>
+                                   <select style="width: 100%;" id="descFrom" name="orig1">
+                                        <?php include'BookFlight-Destinations.php'; ?>
+                                   </select>
+                              </div>
+                              <div class="form-group ">
+               				<label> Number of Passenger</label>
+               				<input class="form-control " type="number" placeholder="PASSENGER"
+                                        min="1" max="10" name="passenger" required>
+               			</div>
+                              <div class="form-group">
+                                   <input type="submit" class="btn btn-primary" name="checkout_out3"/>
+                              </div>
+                         </form>
                     </li>
                </ul>
           </div>
